423eaa91e667410eaa9fda6c8b5d6db852cd8e61
Anton Loukianov <anton.loukianov@gmail.com>
2015-04-07 22:08:19 -0400

    initial bumpversion config

--- nudie/__init__.py
+++ nudie/__init__.py
@@ -16,12 +16,12 @@ from .spectrometer import simple_wavelength_axis, wavelen_to_freq, \
     freq_to_wavelen
 from .utils.winspec import SpeFile
 from .utils.paths import setup_paths
+from .version import version
 from .utils.analysis_bits import tag_phases, cleanup_analogtxt, \
     detect_table_start, synchronize_daq_to_camera, load_analogtxt, \
     determine_shutter_shots, load_camera_file, trim_all, \
     make_6phase_cycler, select_prd_peak
 
-    
 try:
     mount_point, data_folder = setup_paths()
 except RuntimeError as e:
