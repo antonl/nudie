a62a912602d66583c63801c95e4a64cf76764570
Anton Loukianov <anton.loukianov@gmail.com>
2015-04-29 11:37:23 -0400

    Bump version: 0.1.1-dev → 0.2.0-dev

--- nudie/version.py
+++ nudie/version.py
@@ -1,13 +1,13 @@
-version = '0.1.1-dev'
-
-import re
-
-def parse_version(version):
-    reg = re.compile("(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(\-(?P<release>[a-z]+))?")
-    tmp = reg.match(version)
-
-    version_tuple_rel = (int(tmp.group('major')), int(tmp.group('minor')),
-            int(tmp.group('patch')), tmp.group('release'))
-
-    version_tuple = version_tuple_rel[:3]
-    return version_tuple, version_tuple_rel
+version = '0.2.0-dev'
+
+import re
+
+def parse_version(version):
+    reg = re.compile("(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(\-(?P<release>[a-z]+))?")
+    tmp = reg.match(version)
+
+    version_tuple_rel = (int(tmp.group('major')), int(tmp.group('minor')),
+            int(tmp.group('patch')), tmp.group('release'))
+
+    version_tuple = version_tuple_rel[:3]
+    return version_tuple, version_tuple_rel
