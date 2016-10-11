bc7c08efa904b2df106d7b6b6e9ef1a286082e19
Anton Loukianov <aloukian@umich.edu>
2016-10-11 14:57:30 -0400

    finished linear stark, thinking about transformation class

--- nudie/dataset.py
+++ nudie/dataset.py
@@ -6,7 +6,6 @@ import re
 import traitlets
 import nudie
 import itertools as it
-from traitlets import HasTraits
 from traitlets.config.configurable import Configurable
 from traitlets.config import Config
 from dask.delayed import delayed
@@ -14,8 +13,11 @@ from collections import namedtuple
 from contextlib import contextmanager
 import numpy as np
 import numpy.ma as ma
+from scipy.io import loadmat
 
 from .utils.batch_loader import load_job
+
+axis = namedtuple('axis', ['label', 'dim', 'data'])
 
 class RawData(Configurable):
     '''smallest unit of spectroscopy data that can be processed
@@ -105,7 +107,25 @@ class RawData(Configurable):
 
         return self
 
-class Dataset(HasTraits):
+class Transformation(Configurable):
+    def __init__(self, *args, **kwargs):
+        super(Configurable, self).__init__(self, *args, **kwargs)
+        
+    def apply(self, dset, other=[], **kwargs):
+        return dset
+
+class AttachWavelengthAxis(Transformation):
+    def __init__(self, *args, **kwargs):
+        super(Configurable, self).__init__(self, *args, **kwargs)
+        
+    def apply(self, dset, other=[], **kwargs):
+        pass
+ 
+class SpectralInterferometery(Transformation):
+    def apply(self, dset, other=[]):
+        pass
+
+class Dataset(Configurable):
     type = 'Dataset'
 
     phase_cycles = ['frame']
@@ -113,10 +133,11 @@ class Dataset(HasTraits):
     path = traitlets.Unicode(help='location of the dataset on disk', 
         config=True, allow_none=False)
 
-    mode = traitlets.Enum(['r', 'r+', 'a'], default_value='r', 
+    mode = traitlets.Enum(['r', 'r+', 'a', 'w', 'w-', 'x'], default_value='r', 
         config=True)
 
-    allow_parallel = traitlets.Bool(default_value=False, config=True)
+    allow_parallel = traitlets.Bool(default_value=False, config=True)    
+    axes = traitlets.List(default_value=[])
 
     def __init__(self, *args, **kwargs):
         '''open hdf5 file representing spectroscopy data
@@ -134,30 +155,131 @@ class Dataset(HasTraits):
             config = Config(config)
 
         super(Dataset, self).__init__(config=config, **kwargs)
-
         new_mode = kwargs.pop('mode', None)
         if new_mode: 
             self.mode = new_mode
-
-        path = pathlib.Path(self.path)
-        log.debug('created dataset `{path!s}` ({type:s})'.format(
-            type=self.type, 
-            path=path)
-            )
-
+    
     def from_raw_data(self, iterable):
-        pass
+        return self
 
-    def load(self):
-        pass
+    def load(self, path=None):
+        if path:
+            self.path = path
 
-    def save(self):
+        with self._raw_hdf5() as h5file:
+            result = self._loadhook(h5file)
+            
+        return result
+
+    def _loadhook(self, h5file):
         pass
+    
+    def save(self, path=None, overwrite=False):
+        if path:
+            self.path = path
+        
+        oldmode = self.mode
+        self.mode = 'w' if overwrite else self.mode
+        
+        with self._raw_hdf5() as h5file:
+            result = self._savehook(h5file)
+            
+        self.mode = oldmode
+        return result
+    
+    def _savehook(self, h5file):
+        '''hook in superclass that is called upon file save
+        '''
+        pass
+    
+    def compute(self):
+        '''dummy method to allow use with or without parallel
+        '''
+        return self
+    
+    def transform(self, transform, *args, **kwargs):
+        '''apply a transformation to the dataset to return a new dataset'''
+        xfm = delayed(transform) if self.allow_parallel else transform
+        return xfm(self, *args, **kwargs)
+        
+    @traitlets.validate('path')
+    def _validate_batch_path(self, proposal):
+        res = pathlib.Path(proposal['value'])
+        return str(res.absolute())
 
     @contextmanager
-    def raw_hdf5(self):
+    def _raw_hdf5(self):
         with h5py.File(self.path, self.mode) as sf:
             yield sf
+
+class WavelengthCalibration(Dataset):
+    phase_cycles = [] # unused
+    
+    calibration_coefficients = traitlets.Instance(klass=np.ndarray,
+        help='coefficients of polynomial used to evaluate pixel values')
+        
+    calibration_type = traitlets.Enum(['polynomial fit', 'unknown'], 
+        help='method used to get calibration')
+    
+    def __init__(self, *args, **kwargs):
+        super(WavelengthCalibration, self).__init__(**kwargs)
+        
+        
+    @classmethod
+    def from_mat_file(klass, path):
+        path = pathlib.Path(path)
+        data = loadmat(str(path))
+        inst = klass()        
+        inst.calibrated_wavelength = np.squeeze(data['saved_wavelengths'])     
+        px = np.arange(inst.calibrated_wavelength.shape[0], dtype=int)
+        px_ax = axis(label='pixels', dim=0, data=px)
+        inst.axes.append(px_ax)
+        inst.path = str(path.with_suffix('.h5').absolute())
+        inst.calibration_type = 'polynomial fit'
+        inst.calibration_coefficients = np.squeeze(data['saved_calib_p'])
+                
+        return inst
+    
+    def _savehook(self, h5file):
+        #gax = h5file.require_group('axes')
+        data = h5file.require_dataset('calibrated wavelength', 
+                            dtype=self.calibrated_wavelength.dtype,
+                            shape=self.calibrated_wavelength.shape,
+                            data=self.calibrated_wavelength)
+        
+        # save attributes
+        for name, trait in self.class_own_traits().items():
+            data.attrs[name] = trait.get(self)
+        
+        gax = h5file.require_group('axes')
+        for ax in self.axes:                    
+            fax = gax.require_dataset(ax.label, 
+                                      shape=ax.data.shape, 
+                                      dtype=ax.data.dtype,
+                                      data=ax.data)
+            data.dims[ax.dim].label = ax.label
+            data.dims.create_scale(fax)
+            data.dims[ax.dim].attach_scale(fax)
+    
+    def _loadhook(self, h5file):
+        data = h5file['calibrated wavelength']
+        
+        for name, trait in self.class_own_traits().items():
+            trait.set(self, data.attrs[name])
+            
+        self.calibrated_wavelength = np.array(data)
+        
+        gax = h5file.get('axes', default=None)
+        if not gax:
+            return # no axes to load
+            
+        for i,dim in enumerate(data.dims):
+            if len(dim) < 1: # empty dim
+                continue
+            
+            a = axis(label=dim.label, dim=i, data=np.array(dim[0]))
+            if a.label not in [item.label for item in self.axes]:
+                self.axes.append(a)        
     
 class PP(Dataset):
     type = 'pump-probe'
@@ -178,13 +300,13 @@ class LinearStark(Dataset):
 
     dynamic_range = traitlets.Float(default_value=1e-7, 
         help='fraction of maximum value of reference intensity to mask, ' \
-                + 'to avoid division by zero') 
-
+                + 'to avoid division by zero')
+                
+    linear_stark_meta = namedtuple('meta', 
+        ['t2', 'table', 'loop', 'voltage_on', 'voltage_off'])
+    
     def __init__(self, *args, **kwargs):
         super(LinearStark, self).__init__(*args, **kwargs)
-        
-        self._axis = None
-        self._axis_label = None
 
     def from_raw_data(self, iterable):
         # define delayed functions if allowing parallel execution
@@ -199,9 +321,11 @@ class LinearStark(Dataset):
             tags = rd.tags
             nrepeat = rd.nrepeat
             nwaveforms = rd.nwaveforms
+            camera_data = delayed(rd.camera_data) if self.allow_parallel else rd.camera_data
+            a2 = delayed(rd.analog_channels.a2) if self.allow_parallel else rd.analog_channels.a2 
 
-            stark_idx = npw(rd.analog_channels.a2 > self.field_on_threshold_volts)[0]
-            nostark_idx = npw(rd.analog_channels.a2 <= self.field_on_threshold_volts)[0]
+            stark_idx = npw(a2 > self.field_on_threshold_volts)[0]
+            nostark_idx = npw(a2 <= self.field_on_threshold_volts)[0]
 
             tmp = [] # holds stark spectra from different phases
             meta = [] # holds debug information for stark spectra (t2, table, loop)
@@ -221,18 +345,18 @@ class LinearStark(Dataset):
                          assume_unique=True)                   
                             
                 # remove probe scatter for stark shots
-                field_on_frames = rd.camera_data[:, so].mean(axis=-1) \
-                        - rd.camera_data[:, sc].mean(axis=-1)
+                field_on_frames = camera_data[:, so].mean(axis=-1) \
+                        - camera_data[:, sc].mean(axis=-1)
 
                 # and for no-stark shots
-                field_off_frames = rd.camera_data[:, no].mean(axis=-1) \
-                    - rd.camera_data[:, nc].mean(axis=-1)
+                field_off_frames = camera_data[:, no].mean(axis=-1) \
+                    - camera_data[:, nc].mean(axis=-1)
                 
                 # mask small values to avoid division by zero
                 field_off_frames_masked = ml(field_off_frames,
                         self.dynamic_range*field_off_frames.max())
 
-                stark_spec = l10(field_on_frames/field_off_frames)
+                stark_spec = l10(field_on_frames/field_off_frames_masked)
                 # fill masked areas with zero
                 stark_spec_filled = mf(stark_spec, 0)
                 tmp.append(stark_spec_filled)
@@ -241,17 +365,81 @@ class LinearStark(Dataset):
             # identical
             if self.allow_parallel:
                 avg_spectrum = delayed(sum)(tmp)/len(tmp)
+                tmp_meta = delayed(self.linear_stark_meta)(rd.t2, rd.table, rd.loop, 
+                    a2[stark_idx].mean(),
+                    a2[nostark_idx].mean())
             else:
                 avg_spectrum = sum(tmp)/len(tmp)
+                tmp_meta = self.linear_stark_meta(rd.t2, rd.table, rd.loop, 
+                    a2[stark_idx].mean(),
+                    a2[nostark_idx].mean())
 
-            stark_spectra.append(avg_spectrum)
-            meta.append((rd.t2, rd.table, rd.loop))
+            stark_spectra.append(avg_spectrum)            
+            meta.append(tmp_meta)
 
-        self.stark_spectra = stark_spectra
-        self.meta = meta
-
+        if self.allow_parallel:
+            self.stark_spectra = [s.compute() for s in stark_spectra]
+            self.meta = [m.compute() for m in meta]
+        else:
+            self.stark_spectra = stark_spectra
+            self.meta = meta
+        
+        # create voltage axis
+        voltage_data = np.array([m.voltage_on - m.voltage_off for m in self.meta])
+        voltage_axis = axis(label='voltage', dim=0, data=voltage_data)
+        self.axes.append(voltage_axis)
+        
         return self
+    
+    def _savehook(self, h5file):
+        nspectra = len(self.stark_spectra)
 
+        if nspectra < 1:
+            log.debug('nothing to save')            
+            return
+        
+        shape = self.stark_spectra[0].shape
+        data = h5file.require_dataset('stark spectra', 
+                shape=(nspectra, shape[0]), dtype=np.float64)
+        
+        for i,s in enumerate(self.stark_spectra):
+            data[i] = s
+            
+        # save attributes
+        for name, trait in self.class_own_traits().items():
+            data.attrs[name] = trait.get(self)
+        
+        gax = h5file.require_group('axes')
+        for ax in self.axes:                    
+            fax = gax.require_dataset(ax.label, 
+                                      shape=ax.data.shape, 
+                                      dtype=ax.data.dtype,
+                                      data=ax.data)
+            data.dims[ax.dim].label = ax.label
+            data.dims.create_scale(fax)
+            data.dims[ax.dim].attach_scale(fax)
+    
+    def _loadhook(self, h5file):
+        print('called loadhook')
+        data = h5file['stark spectra']
+        
+        for name, trait in self.class_own_traits().items():
+            trait.set(self, data.attrs[name])
+            
+        self.stark_spectra = np.array(data)
+        
+        gax = h5file.get('axes', default=None)
+        if not gax:
+            return # no axes to load
+            
+        for i,dim in enumerate(data.dims):
+            if len(dim) < 1: # empty dim
+                continue
+            
+            a = axis(label=dim.label, dim=i, data=np.array(dim[0]))
+            if a.label not in [item.label for item in self.axes]:
+                self.axes.append(a)
+         
 class DD(Dataset):
     type = '2DES'
 
@@ -289,8 +477,8 @@ class DatasetLoader(Configurable):
         log.debug('created dataset loader `{!s}`'.format(self.type))
 
     def load(self, path, type, *args, **kwargs):
-        # Load in setup specific way and return a dataset object
-        return RawData()
+        # Load in setup specific way and return an iterable of RawData
+        return []
 
 class FastDazzlerSetupLoader(DatasetLoader):
     type = 'FastDazzler'
@@ -347,7 +535,7 @@ class FastDazzlerSetupLoader(DatasetLoader):
 
             c = Config()
 
-            # copy over settings used
+            # copy over settings used to RawData
             c.RawData.batch_name = self.batch_name
             c.RawData.batch_path = self.batch_path
             c.RawData.nwaveforms = self.nwaveforms
@@ -357,6 +545,9 @@ class FastDazzlerSetupLoader(DatasetLoader):
             c.RawData.loop = loop
             c.RawData.phase_cycles = self.data_type.phase_cycles
 
+            c.Dataset.allow_parallel = self.allow_parallel
+            c.merge(self.config)
+
             if self.allow_parallel:
                 raw_datas.append(delayed(RawData)(config=c).process())
             else:
@@ -364,8 +555,8 @@ class FastDazzlerSetupLoader(DatasetLoader):
 
         # create dataset and process it
         if self.allow_parallel:
-            dset = delayed(self.data_type)(config=self.config)
+            dset = delayed(self.data_type)(config=c)
         else:
-            dset = self.data_type(config=self.config)
+            dset = self.data_type(config=c)
 
         return dset.from_raw_data(raw_datas)
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,6 +1,6 @@
 import nudie
-from nudie.dataset import *
-from dask.delayed import delayed
+from nudie.dataset import (LinearStark, FastDazzlerSetupLoader, Config,
+                           WavelengthCalibration)
 
 nudie.show_errors(nudie.logging.INFO)
 
@@ -21,8 +21,8 @@ TGStark('tg-stark-batch00.h5')
 '''
 c = Config(
     {'FastDazzlerSetupLoader': 
-        {'batch_path': 'test-data/clh-linear-stark-s3-batch00',
-         'batch_name': 'clh-linear-stark-s3',
+        {'batch_path': 'C:/aloukian-project/16-10-10/test-data/tips-3mp-gly-77k-linear-stark-batch00',
+         'batch_name': 'tips-3mp-gly-77k-linear-stark',
          'data_type': LinearStark,
          'allow_parallel': True,
         }})
@@ -33,10 +33,18 @@ res = a.compute()
 
 #res = a
 
-import matplotlib.pyplot
-matplotlib.pyplot.plot(res.stark_spectra[0])
-matplotlib.pyplot.show()
+#import matplotlib.pyplot as plt
+#plt.plot(res.stark_spectra[0])
+#plt.tight_layout()
+#plt.show()
 
+res.path = 'C:/aloukian-project/16-10-10/tips-3mp-gly-77k-linear-stark.h5'
+res.save(overwrite=True)
+
+wl = WavelengthCalibration.from_mat_file('C:/aloukian-project/16-10-10/calib-spec-600g-605nm.mat')
+wl.save(overwrite=True)
+wl2 = WavelengthCalibration()
+wl2.load('C:/aloukian-project/16-10-10/calib-spec-600g-605nm.h5')
 '''
 
 from traitlets.config import Config
